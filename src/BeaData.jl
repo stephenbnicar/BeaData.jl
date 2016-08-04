dceef416badb173fcb72aea4b08fe0f20f3d9ad7
Stephen Nicar <snicar@fandm.edu>
2016-08-04 13:52:50 -0400

    Initial version

--- src/BeaData.jl
+++ src/BeaData.jl
@@ -1,5 +1,115 @@
+isdefined(Base, :__precompile__) && __precompile__()
+
 module BeaData
 
-# package code goes here
+using Requests
+using DataFrames, DataStructures
+
+export
+    Bea,
+    BeaNipaTable,
+    api_url,
+    api_key,
+    api_dataset,
+    get_nipa_table
+
+const DEFAULT_API_URL   = "http://www.bea.gov/api/data"
+const API_KEY_LENGTH    = 36
+const DEFAULT_DATASET   = "NIPA"
+
+"""
+A connection to the U.S. Bureau of Economic Analysis (BEA) Data API.
+
+Constructors
+------------
+* `Bea()`
+* `Bea(key::AbstractString)`
+
+Arguments
+---------
+* `key`: Registration key provided by the BEA.
+
+* A valid registration key is *required* to retrieve data from the BEA's API.
+  A key can be obtained by registering at the BEA website.
+
+* A default API key can be specified in a ~/.beadatarc file.
+
+Methods
+-------
+* `api_url(b::Bls)`: Get the base URL used to connect to the server
+* `api_key(b::Bls)`: Get the API key
+
+"""
+type Bea
+    url::AbstractString
+    key::AbstractString
+    dataset::AbstractString
+end
+
+function Bea(key="")
+    if isempty(key)
+        try
+            open(joinpath(homedir(),".beadatarc"), "r") do f
+                key = readall(f)
+            end
+            key = rstrip(key)
+            @printf "API key loaded.\n"
+            # Key validation
+            if length(key) > API_KEY_LENGTH
+                key = key[1:API_KEY_LENGTH]
+                warn("Key too long. First ", API_KEY_LENGTH, " chars used.")
+            end
+        catch err
+        end
+
+    end
+
+    url = DEFAULT_API_URL
+    dataset = DEFAULT_DATASET
+    Bea(url, key, dataset)
+end
+
+api_url(b::Bea) = b.url
+api_key(b::Bea) = b.key
+api_dataset(b::Bea) = b.dataset
+
+function Base.show(io::IO, b::Bea)
+    @printf io "BEA API Connection\n"
+    @printf io "\turl: %s\n" api_url(b)
+    @printf io "\tkey: %s\n" api_key(b)
+    @printf io "\tdataset: %s\n" api_dataset(b)
+end
+
+# """
+# A NIPA table with metadata returned from a `get_nipa_table` call.
+#
+# Access fields with
+# ```
+# id(s::BeaNipaTable)
+# series(s::BeaNipaTable)
+# notes(s::BeaNipaTable)
+# ```
+# """
+type BeaNipaTable
+    tablenum::AbstractString
+    tabledesc::AbstractString
+    linedesc::OrderedDict
+    tablenotes::OrderedDict
+    frequency::AbstractString
+    startyear::Int
+    endyear::Int
+    df::DataFrame
+end
+EMPTY_RESPONSE() = BeaNipaTable("", "", "", 0, 0, DataFrame())
+
+function Base.show(io::IO, b::BeaNipaTable)
+    @printf io "BEA NIPA Table\n"
+    @printf io "\tTable: %s\n" b.tablenum
+    @printf io "\tDescription: %s\n" b.tabledesc
+    @printf io "\tCoverage: %s, from %s to %s\n" b.frequency b.startyear b.endyear
+end
+
+include("get_nipa_table.jl")
+
 
 end # module
---
+++ src/get_nipa_table.jl
@@ -0,0 +1,95 @@
+"""
+```
+    get_nipa_table(b::Bea, TableID::Int, frequency::AbstractString,
+        startyear::Int, endyear::Int)
+```
+
+Request a NIPA table from the BEA data API.
+
+Arguments
+---------
+* `b`: a BEA API connection
+* `TableID`: the integer Table ID for the desired NIPA table
+* `frequency`: "A" for annual, "Q" for quarerly
+* `startyear`: first year of data requested, in YYYY format
+* `endyear`: last year of data requested, in YYYY format
+
+Returns
+-------
+An object of type `BeaNipaTable`
+
+"""
+function get_nipa_table(b::Bea, TableID, frequency, startyear, endyear)
+    url = api_url(b)
+    key = api_key(b)
+    dataset = api_dataset(b)
+    bea_method = "GetData"
+    years = collect(startyear:1:endyear)
+
+    querydict = Dict("UserID" => key,
+                     "Method" => bea_method,
+                     "DatasetName" => dataset,
+                     "TableID" => TableID,
+                     "Frequency" => frequency,
+                     "Year" => years,
+                     "ResultFormat" => "JSON")
+
+    response = get(url; query = querydict)
+    response_json = Requests.json(response)
+
+    # Extract the vector of Dicts containing the data
+    data_dicts = response_json["BEAAPI"]["Results"]["Data"]
+    # Retrieve data from each Dict
+    all_data = [parse_data_dict(d) for d in data_dicts]
+    values = [tup[1] for tup in all_data]
+    dates = [tup[2] for tup in all_data]
+    linenums = [tup[3] for tup in all_data]
+    # Create data frame of values, reshape, and create names
+    dflong = DataFrame(date = dates, line = linenums, value = values)
+    df = unstack(dflong, :date, :line, :value) # Convert to "wide" format
+    newnames = [symbol(string("line", name)) for name in names(df)[2:end]]
+    names!(df, [:date; newnames])
+
+    # Create OrderedDict of line descriptions
+    linedesc = [tup[4] for tup in all_data]
+    linekeys = OrderedDict{AbstractString, AbstractString}(zip(linenums, linedesc))
+
+    # Extract metadata for the table
+    tablenotes = response_json["BEAAPI"]["Results"]["Notes"]
+    tableinfo = split(tablenotes[1]["NoteText"])
+    tablenum = rstrip(tableinfo[2], '.')
+    tabledesc = join(tableinfo[3:end], " ")
+    if length(tablenotes) > 1
+        noteref = [note["NoteRef"] for note in tablenotes[2:end]]
+        notetext = [note["NoteText"] for note in tablenotes[2:end]]
+        notes = OrderedDict{AbstractString, AbstractString}(zip(noteref, notetext))
+    else
+        notes = ""
+    end
+
+    frequency == "Q"? freq = "Quarterly": freq = "Annual"
+    out = BeaNipaTable(tablenum, tabledesc, linekeys, notes, freq, startyear, endyear, df)
+    return out
+end
+
+function parse_data_dict{T<:AbstractString}(dict::Dict{T,Any})
+    # function extracts values from data dictionary and returns a tuple
+    linenum = dict["LineNumber"]
+    linedesc = dict["LineDescription"]
+    dataval = float(replace(dict["DataValue"], ",", ""))
+
+    # Change date from string to Date() type
+    timeperiod = dict["TimePeriod"]
+    year = parse(Int, timeperiod[1:4])
+    # Quarterly data
+    if ismatch(r"Q", timeperiod)
+        quarter = parse(Int, timeperiod[end])
+        quarter in (1, 4)? day = 31 : day = 30
+        date = Date(year, quarter*3, day)
+    # Annual data
+    else
+        date = Date(year, 12, 31)
+    end
+
+    return (dataval, date, linenum, linedesc)
+end
