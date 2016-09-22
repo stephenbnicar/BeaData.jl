db9e929e7cda9bda164ee638dd023bf9324894ea
Stephen Nicar <snicar@fandm.edu>
2016-09-21 20:39:19 -0400

    Add DocStringExtensions to REQUIRE file

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Base.Test
 
 try
     open(joinpath(homedir(),".beadatarc"), "r") do f
-        key = readall(f)
+        key = readstring(f)
     end
     key = rstrip(key)
     include("test_with_key.jl")
