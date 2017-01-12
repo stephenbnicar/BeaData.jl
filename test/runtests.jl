2e5bbbc2264767851a04bd30d9def8c59738e393
Stephen Nicar <stephenbnicar@gmail.com>
2017-01-12 14:42:21 -0500

    Testing encrypted travis key

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,15 @@ using BeaData
 using Base.Test
 using Compat
 
-try
-    open(joinpath(homedir(),".beadatarc"), "r") do f
-        key = readstring(f)
-    end
-    key = rstrip(key)
-    include("test_with_key.jl")
-catch err
-end
+# try
+#     open(joinpath(homedir(),".beadatarc"), "r") do f
+#         key = readstring(f)
+#     end
+#     key = rstrip(key)
+#     include("test_with_key.jl")
+# catch err
+# end
+
+key = ENV["BEA_KEY"]
+b = Bea(key)
+nipatable = get_nipa_table(b, 5, "Q", 2014, 2014)
