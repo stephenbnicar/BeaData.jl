b1cb2c24df77580ea201bfa081ab3fd0188ebc6b
Stephen Nicar <stephenbnicar@gmail.com>
2019-04-17 21:36:43 -0400

    Update to handle "broken pipe" error (#11)
    Resolving issue #10

--- src/get_bea_parameterlist.jl
+++ src/get_bea_parameterlist.jl
@@ -14,33 +14,13 @@ function get_bea_parameterlist(b::Bea, dataset::String)
     key = b.key
     bea_method = "GetParameterList"
 
-    query = string(url,
-                "?&UserID=", key,
-                "&method=", bea_method,
-                "&DatasetName=", dataset,
-                "&ResultFormat=JSON&")
+    querydict = Dict("UserID" => key,
+                     "Method" => bea_method,
+                     "DatasetName" => dataset,
+                     "ResultFormat" => "JSON")
 
-    response = HTTP.get(query)
-    response_body = String(response.body)
-    response_json = JSON.parse(response_body)
+    response_json = bea_query(url, querydict)
     response_dict = response_json["BEAAPI"]["Results"]["Parameter"]
-
-    # parameter_id = String[]
-    # parameter_desc = String[]
-    # parameter_req = String[]
-    # default_value = String[]
-    # for dict in response_dict
-    #     push!(parameter_id, dict["ParameterName"])
-    #     push!(parameter_desc, dict["ParameterDescription"])
-    #     push!(parameter_req, dict["ParameterIsRequiredFlag"])
-    #     if haskey(dict, "ParameterDefaultValue")
-    #         push!(default_value, dict["ParameterDefaultValue"])
-    #     else
-    #         push!(default_value, "")
-    #     end
-    # end
-    # parameter_list = DataFrame(parameter_id = parameter_id, parameter_description = parameter_desc,
-    #                             required = parameter_req, default_value = default_value)
-
+    
     return response_dict
 end
