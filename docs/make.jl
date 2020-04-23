714261dc5198f7907a8a611da0eaf21444e7ba13
Stephen Nicar <stephenbnicar@gmail.com>
2020-04-22 20:19:49 -0500

    Updated docs/make.jl with deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,6 @@ makedocs(
     modules = [BeaData]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/stephenbnicar/BeaData.jl.git"
+)
