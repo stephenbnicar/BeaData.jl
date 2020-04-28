ce81441b5ef05611247e1d46bf6d1abfd104e76d
Stephen Nicar <stephenbnicar@gmail.com>
2020-04-28 12:00:54 -0500

    Added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,24 @@
 using BeaData
 using Test
 
-# dataset_list = bea_datasets()
-# parameter_list = bea_parameterlist("NIPA")
+@testset "metadata methods" begin
+    ds = bea_datasets()
+    @test "NIPA" ∈ ds[!, :DatasetName]
+
+    pl = bea_parameterlist("FixedAssets")
+    @test "TableName" ∈ pl[!, :ParameterName]
+
+    pv = bea_parametervalues("NIUnderlyingDetail", "TableName")
+    @test "U001A" ∈ pv[!, :Value]
+end
+
+@testset "data retrieval" begin
+    nipa111 = bea_table("NIPA", "T10101", "A", 0 , 0)
+    @test typeof(nipa111) <: BeaTable
+
+    ud1a = bea_table("NIUnderlyingDetail", "U001A", "Q", 1996, 1996)
+    @test typeof(ud1a) <: BeaTable
+
+    faat101 = bea_table("FixedAssets", "FAAt101", "A", 0, 0)
+    @test typeof(faat101) <: BeaTable
+end
